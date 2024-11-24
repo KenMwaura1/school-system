d2f19bd0ced0dfb57ce69f3865e116825522cef8
KenMwaura1 <kemwaura@gmail.com>
2024-11-24 13:44:56 +0300

    Update dashboard to redirect based on user session and add Assign Subjects link

--- dashboard.php
+++ dashboard.php
@@ -1,11 +1,12 @@
 <?php
 session_start();
 
-// Check if user is logged in
-if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
-    header('Location: login.html');
-    exit;
+if (!isset($_SESSION['user_id'])) {
+    header("Location: login.php");
+    exit();
 }
+
+$user_name = $_SESSION['user_name'];
 
 // Check if user is a student
 if ($_SESSION['user_role'] !== 'student') {
@@ -167,14 +168,7 @@ try {
         <div class="flex-none gap-2">
             <a href="index.html" class="btn btn-ghost">Home</a>
             <a href="?logout=true" class="btn btn-ghost">Logout</a>
-            <button class="btn btn-ghost btn-circle">
-                <div class="indicator">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
-                    </svg>
-                    <span class="badge badge-xs badge-primary indicator-item"></span>
-                </div>
-            </button>
+            <a href="assign_subject_form.php" class="btn btn-ghost">Assign Subjects</a>
         </div>
     </div>
 <br>
@@ -280,3 +274,7 @@ try {
                 <?php } ?>
             </div>
         </div>
+</body>
+</html>
+
+
